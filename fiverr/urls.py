367753e5b1960b3300f027576ac8a40d90d069c6
Samim Osman Sabuj <86197373+samimosmansabuj@users.noreply.github.com>
2025-10-01 16:53:57 +0600

    url update!!!

--- fiverr/urls.py
+++ fiverr/urls.py
@@ -3,7 +3,7 @@ from .views import fiverr_data, ScrapFiverrDataView, result, get_subcategories, 
 
 urlpatterns = [
     path('fiverr/data-list/', fiverr_data, name="fiverr_data"),
-    path('fiverr/scrap_new_data/', ScrapFiverrDataView.as_view(), name="scrap_fiverr_data"),
+    path('fiverr/scrap-new-data/', ScrapFiverrDataView.as_view(), name="scrap_fiverr_data"),
     path('fiverr/result/', result, name="result"),
     path('fiverr/verify/', verify_fiverr_url, name="verify_fiverr_url"),
     
